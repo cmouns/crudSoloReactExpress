3c7c27772b7dbd707791ea65a28dbb5c720271db
cmouns <mounir.sebti33@email.com>
2025-10-02 09:49:35 +0200

    Init cors & corsOptions

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 import express from "express";
+import cors from "cors";
 
 
 const app = express();
 const port = 8081;
+
+const corsOptions = {
+  origin: ['http://localhost:3000', 
+           'http://localhost:8081'
+        ],
+  optionsSuccessStatus: 200,// some legacy browsers (IE11, various SmartTVs) choke on 204
+  methods: ['GET','POST','PUT','DELETE'],
+  allowedHeaders : ['Content-Type', 'Authorization'],
+  credentials: true,
+}
+
 
 app.listen(port,() => {
     console.log(`Server is running on port : ${port}`);
