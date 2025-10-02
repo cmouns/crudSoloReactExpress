06ece6b0f05e84f5d8d7f02f9a6076d3c24c623b
cmouns <mounir.sebti33@email.com>
2025-10-02 10:43:31 +0200

    Init Student component with axios

--- frontend/src/components/Student.jsx
+++ frontend/src/components/Student.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 
 
 function Student() {
-    
+    const [student, setStudent] = useState([]);
+    useEffect(()=>{
+        axios.get('http://localhost:8081/')
+        .then(res => setStudent(res.data))
+        .catch(err => console.log(err)
+        )
+    })
+
   return (
     <div>
         <h1>test</h1>
