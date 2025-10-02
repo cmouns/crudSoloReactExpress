41fd5de5ca85f1844827bcca349e62b5ff10c6a6
cmouns <mounir.sebti33@email.com>
2025-10-02 11:29:38 +0200

    Give data into component Student + init TailwindCSS

--- backend/server.js
+++ backend/server.js
@@ -8,26 +8,36 @@ const port = 8081;
 
 // Permet d'établir la connexion et d'autoriser la connexion entre le front-end (en port 3000) et le backend (en port 8081)
 const corsOptions = {
-  origin: ['http://localhost:3000', // Autorise les connexions contenant ces URL
-           'http://localhost:8081'
+  origin: ['http://localhost:5173', // Autorise les connexions contenant ces URL
+           'http://localhost:8081',
+           'http://localhost:3000',
         ],
   optionsSuccessStatus: 200,// Renvoi le status 200 si la connexion est bien établis
-  methods: ['GET','POST','PUT','DELETE'], // Permet d'autoriser les requetes faites par le front-end contenant ces méthodes.
-  allowedHeaders : ['Content-Type', 'Authorization'], // Content-Type : Permet d'envoyer des requetes avec le body en JSON, Authorization : Permet d'envoyer des requetes avec le token dans le header
+  methods: 'GET,POST,PUT,DELETE', // Permet d'autoriser les requetes faites par le front-end contenant ces méthodes.
+  headers : 'Content-Type, Authorization', // Content-Type : Permet d'envoyer des requetes avec le body en JSON, Authorization : Permet d'envoyer des requetes avec le token dans le header
   credentials: true, // Permet d'envoyer des cookies (utile pour la gestion de session)
 }
 
-const database= await mysql.createConnection({
+app.use(express.json()); // Permet de parser le body des requetes en JSON
+app.use(cors(corsOptions)); // Utilisation du middleware CORS avec les options définies
+
+const database=  mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'cda',
 })
 
+app.get("/", (req,res)=>{
+    const sql = "SELECT * FROM role";
+    database.query(sql, (err,data) => {
+        if(err) {
+            return res.status(500).json({error: "Database error"});
+        }
+        return res.json(data);
+    })
 
-app.use(cors(corsOptions)); // Utilisation du middleware CORS avec les options définies
-app.use(express.json()); // Permet de parser le body des requetes en JSON
-
+})
 
 app.listen(port,() => {
     console.log(`Server is running on port : ${port}`);
--- frontend/src/components/Student.jsx
+++ frontend/src/components/Student.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect}  from 'react'
 import axios from 'axios';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import {Link} from 'react-router-dom';
 
 
 
@@ -10,13 +9,35 @@ function Student() {
     useEffect(()=>{
         axios.get('http://localhost:8081/')
         .then(res => setStudent(res.data))
+        
         .catch(err => console.log(err)
         )
-    })
-
+    }, [])
+    
   return (
-    <div>
-        <h1>test</h1>
+    <div className="relative overflow-x-auto">
+        <Link to="/add" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Ajouter</Link>
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">Prenom</th>
+                    <th scope="col" class="px-6 py-3">Rôle</th>
+                    
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    student.map((data, i)=> (
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200" key={i}>
+                            <td className= "px-6 py-4">{data.name}</td>
+                            <td className= "px-6 py-4">{data.role}</td>
+                        </tr>
+                    ))
+                }
+                
+
+            </tbody>
+            </table>
     </div>
   )
 }
