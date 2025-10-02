56bab5aca116aa1a086cb287ea6dbfc5d7a33e46
cmouns <mounir.sebti33@email.com>
2025-10-02 09:55:45 +0200

    Add comment to understand corsOptions

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,19 @@ import cors from "cors";
 const app = express();
 const port = 8081;
 
+// Permet d'établir la connexion et d'autoriser la connexion entre le front-end (en port 3000) et le backend (en port 8081)
 const corsOptions = {
-  origin: ['http://localhost:3000', 
+  origin: ['http://localhost:3000', // Autorise les connexions contenant ces URL
            'http://localhost:8081'
         ],
-  optionsSuccessStatus: 200,// some legacy browsers (IE11, various SmartTVs) choke on 204
-  methods: ['GET','POST','PUT','DELETE'],
-  allowedHeaders : ['Content-Type', 'Authorization'],
-  credentials: true,
+  optionsSuccessStatus: 200,// Renvoi le status 200 si la connexion est bien établis
+  methods: ['GET','POST','PUT','DELETE'], // Permet d'autoriser les requetes faites par le front-end contenant ces méthodes.
+  allowedHeaders : ['Content-Type', 'Authorization'], // Content-Type : Permet d'envoyer des requetes avec le body en JSON, Authorization : Permet d'envoyer des requetes avec le token dans le header
+  credentials: true, // Permet d'envoyer des cookies (utile pour la gestion de session)
 }
+
+app.use(cors(corsOptions)); // Utilisation du middleware CORS avec les options définies
+app.use(express.json()); // Permet de parser le body des requetes en JSON
 
 
 app.listen(port,() => {
